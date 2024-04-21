8f4990a1e2fbbfed456ae3abd85d926bb15e48ee
Mykyta Tishkin <87891550+GTeasera@users.noreply.github.com>
2024-04-22 01:06:51 +0300

    Base CRUD

---
+++ Lab3WebService/Controllers/ShopsController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Lab3WebService.Data;
+using Lab3WebService.Models;
+
+namespace Lab3WebService.Controllers
+{
+    public class ShopsController : Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public ShopsController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Shops
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Shops.ToListAsync());
+        }
+
+        // GET: Shops/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shops = await _context.Shops
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (shops == null)
+            {
+                return NotFound();
+            }
+
+            return View(shops);
+        }
+
+        // GET: Shops/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Shops/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Address,Email")] Shops shops)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(shops);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(shops);
+        }
+
+        // GET: Shops/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shops = await _context.Shops.FindAsync(id);
+            if (shops == null)
+            {
+                return NotFound();
+            }
+            return View(shops);
+        }
+
+        // POST: Shops/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Address,Email")] Shops shops)
+        {
+            if (id != shops.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(shops);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ShopsExists(shops.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(shops);
+        }
+
+        // GET: Shops/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shops = await _context.Shops
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (shops == null)
+            {
+                return NotFound();
+            }
+
+            return View(shops);
+        }
+
+        // POST: Shops/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var shops = await _context.Shops.FindAsync(id);
+            if (shops != null)
+            {
+                _context.Shops.Remove(shops);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ShopsExists(int id)
+        {
+            return _context.Shops.Any(e => e.Id == id);
+        }
+    }
+}
